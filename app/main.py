008757267e25f3f3b4b346c87066e7516a1af6ea
Ruslan Priadko <r.priadko@ukr.net>
2024-12-26 12:45:37 +0200

    add partner devel

--- app/main.py
+++ app/main.py
@@ -12,5 +12,18 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_list = []
+
     for person in people:
-        person_list.append()
+        person_list.append(Person(
+            name=person["name"],
+            age=person["age"]
+        ))
+
+    def add_partner() -> None:
+        nonlocal person
+        for person in people:
+            if person[wife]
+            for listed_person in person_list:
+
+
+    return person_list
