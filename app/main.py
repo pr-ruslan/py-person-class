75a0640baf08317041161a0e50ec41feaf5ffab4
Ruslan Priadko <r.priadko@ukr.net>
2024-12-26 12:24:25 +0200

    Person defined

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = Person(
+            name=self.name,
+            age=self.age
+        )
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person in people:
+        person_list.append()
